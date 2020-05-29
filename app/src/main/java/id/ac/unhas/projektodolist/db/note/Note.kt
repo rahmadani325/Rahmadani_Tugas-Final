06753b6d75daa1d1dd23bd3b192ba068e8a7383b
smansarum@gmail.com <smansarum@gmail.com>
2020-05-29 15:59:44 +0700

    Menghubungkan MainActivity dengan InputNoteActivity

--- app/src/main/java/id/ac/unhas/projektodolist/db/note/Note.kt
+++ app/src/main/java/id/ac/unhas/projektodolist/db/note/Note.kt
@@ -1,20 +1,36 @@
 package id.ac.unhas.projektodolist.db.note
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "note")
 data class Note(
-    @PrimaryKey(autoGenerate = false)
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null,
+
     @ColumnInfo(name = "buat_waktu")
-    val buatWaktu: Date? = null,
+    val buatWaktu: Int? = null,
+
+    @ColumnInfo(name = "str_buat_waktu")
+    val strBuatWaktu: String? = null,
 
     @ColumnInfo(name = "update_waktu")
-    val updateWaktu: Date? = null,
+    val updateWaktu: Int? = null,
 
     @ColumnInfo(name = "tenggat_waktu")
-    val tenggatWaktu: Date? = null,
+    val tenggatWaktu: Int? = null,
+
+    @ColumnInfo(name = "str_tenggat_waktu")
+    val strTenggatWaktu: String? = null,
+
+    @ColumnInfo(name = "tenggat_jam")
+    val tenggatJam: Int? = null,
+
+    @ColumnInfo(name = "str_tenggat_jam")
+    val strTenggatJam: String? = null,
 
     @ColumnInfo(name = "judul")
     val judul: String,
@@ -24,4 +40,5 @@ data class Note(
 
     @ColumnInfo(name = "selesai")
     val isFinished: Boolean? = null
-)
+
+)  : Parcelable
--- app/src/main/java/id/ac/unhas/projektodolist/db/ui/main/InputNoteActivity.kt
+++ app/src/main/java/id/ac/unhas/projektodolist/db/ui/main/InputNoteActivity.kt
@@ -1,22 +1,123 @@
 package id.ac.unhas.projektodolist.db.ui.main
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import androidx.lifecycle.ViewModelProvider
+import id.ac.unhas.projektodolist.R
+import id.ac.unhas.projektodolist.db.note.Note
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
-class InputNoteActivity {
+class InputNoteActivity : AppCompatActivity() {
+    private lateinit var editTextTitle: EditText
+    private lateinit var editTextDate: EditText
+    private lateinit var editTextNote: EditText
+    private lateinit var editTextTime: EditText
+    private lateinit var btnSave: Button
+    private lateinit var noteViewModel: NoteViewModel
+    private var calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_list)
+        setContentView(R.layout.input_note)
         if(supportActionBar != null){
-            supportActionBar?.title = "Tambahkan note"
+            supportActionBar?.title = "Add a Task"
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // Back Button
+
+        editTextTitle = findViewById(R.id.judul_content)
+        editTextDate = findViewById(R.id.tenggat_hari_content)
+        editTextNote = findViewById(R.id.note_content)
+        editTextTime = findViewById(R.id.tenggat_jam_content)
+        btnSave = findViewById(R.id.btn_simpan)
+        noteViewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+
+        editTextDate.setOnClickListener{
+            setDueDate()
+        }
+
+        editTextTime.setOnClickListener {
+            setDueTime()
+        }
+
+        btnSave.setOnClickListener{
+            saveList()
+        }
     }
 
     // Back Button
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         finish()
         return true
+    }
+
+    private fun setDueDate(){
+        val date = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH)
+        val year = calendar.get(Calendar.YEAR)
+
+        // Date picker dialog
+        val dateListener = DatePickerDialog.OnDateSetListener{ view, year, month, date ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DATE, date)
+            editTextDate.setText(SimpleDateFormat("EEE, MMM dd, yyyy").format(calendar.time))
+        }
+
+        DatePickerDialog(this, dateListener, year, month, date).show()
+    }
+
+    private fun setDueTime(){
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            calendar.set(Calendar.HOUR_OF_DAY, hour)
+            calendar.set(Calendar.MINUTE, minute)
+            editTextTime.setText(SimpleDateFormat("HH:mm").format(calendar.time))
+        }
+
+        TimePickerDialog(this, timeSetListener, hour, minute, true).show()
+    }
+
+    private fun saveList(){
+        val current = ZonedDateTime.now(ZoneId.of("+8"))
+        val formatter = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy, HH:mm:ss")
+
+        val strCreatedDate = current.format(formatter)
+        val createdDate = Converter.dateToInt(current)
+
+        val strDueDate = editTextDate.text.toString().trim()
+        val dueDate = Converter.stringDateToInt(strDueDate)
+
+        val strDueHour = editTextTime.text.toString().trim()
+        val dueHour= Converter.stringTimeToInt(strDueHour)
+
+        val title = editTextTitle.text.toString().trim()
+        val note = editTextNote.text.toString().trim()
+
+        NoteViewModel.insertNote(
+            Note(
+                buatWaktu = buatWaktu,
+                strBuatWaktu = strBuatWaktu,
+                judul = judul,
+                tenggatWaktu = tenggatWaktu,
+                tenggatJam = tenggatJam,
+                strTenggatWaktu = strTenggatWaktu,
+                strTenggatJam = strTenggaJam,
+                note = note,
+                isFinished = false
+            )
+        )
+
+        finish()
     }
 }
